2029200073c1f86f3544a2aa82b1ff140204c1ed
Ignacio Cabral <ignaciocabral97@gmail.com>
2024-12-04 14:46:36 -0300

    synthesized decrees

--- index.js
+++ index.js
@@ -32,9 +32,13 @@ async function parsePDF(pdfBuffer) {
         const pdfData = await pdf(pdfBuffer);
         // Obtener el texto en una sola línea
         let text = pdfData.text;
-        text = text.replace(/\s{2,}/g, ' '); //reemplaza 2 o mas espacios por uno
-        text = text.replace(/FRANQUEO A PAGAR\n(.*?)[Ss]ecci[óo]n [Oo]ficial/s, ''); //elimina esta sección
-        text = text.replace(/([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ])-\n([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ])/g, "$1$2"); //junta las palabras separadas por guión
+
+        text = text.replace(/\s{2,}/g, ' ') //reemplaza 2 o mas espacios por uno
+            .replace(/FRANQUEO A PAGAR\n(.*?)[Ss]ecci[óo]n [Oo]ficial/s, '') //elimina esta sección
+            .replace(/FRANQUEO A PAGAR\n(.*?)[Ss]ecci[óo]n [Oo]ficial/s, '') //elimina esta sección
+            .replace(/([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ])-\n([a-zA-ZáéíóúÁÉÍÓÚüÜñÑ])/g, "$1$2") //junta las palabras separadas por guión
+            .replace(/BOLET[IÍ]N OFICIAL\n/g, '')
+            .replace(/P[AÁ]GINA (\d+)\n(Lunes|Martes|Mi[eé]rcoles|Jueves|Viernes|S[aá]bado|Domingo)\s\d{1,2}\sde\s(Enero|Febrero|Marzo|Abril|Mayo|Junio|Julio|Agosto|Septiembre|Octubre|Noviembre|Diciembre)\sde\s\d{4}\n/gs, '');
 
         const sectionsRegex = /(DECRETO PROVINCIAL\n|DECRETOS PROVINCIALES\n|DECRETO SINTETIZADO\n|DECRETOS SINTETIZADOS\n|RESOLUCIONES\n|RESOLUCI[ÓO]N\n|RESOLUCIONES SINTETIZADAS\n|RESOLUCI[ÓO]N SINTETIZADA\n)([\s\S]*?)(?=(DECRETO PROVINCIAL\n|DECRETOS PROVINCIALES\n|DECRETO SINTETIZADO\n|DECRETOS SINTETIZADOS\n|RESOLUCIONES\n|RESOLUCI[ÓO]N\n|RESOLUCIONES SINTETIZADAS\n|RESOLUCI[ÓO]N SINTETIZADA\n|Secci[óo]n General|$))/g;
         const sections = [];
@@ -65,14 +69,16 @@ async function parsePDF(pdfBuffer) {
                     break;
 
                 case 'DECRETOS PROVINCIALES':
-                    let decreeContent = processProvincialDecrees(sectionContent);
-                    content.push(...decreeContent);
+                    const decrees = processProvincialDecrees(sectionContent);
+                    content.push(...decrees);
                     break;
 
                 case 'DECRETO SINTETIZADO':
                     break;
 
                 case 'DECRETOS SINTETIZADOS':
+                    const synthesizedDecrees = processSynthesizedDecrees(sectionContent);
+                    content.push(...synthesizedDecrees);
                     break;
 
                 case 'RESOLUCIONES':
@@ -97,7 +103,7 @@ async function parsePDF(pdfBuffer) {
 
 
 function processProvincialDecrees(sectionContent) {
-    const decree = [];
+    const decrees = [];
     const provincialDecreeRegex = /PODER EJECUTIVO(.*?)(?=PODER EJECUTIVO|$)/gs;
     const decreeTitleRegex = /Decreto N° \d{1,4}\n/g;
 
@@ -107,12 +113,52 @@ function processProvincialDecrees(sectionContent) {
         let titleMatch;
 
         while ((titleMatch = decreeTitleRegex.exec(decreeContent)) !== null) {
-            decree.push({
+            decrees.push({
                 title: 'Auditoría Legislativa - ' + 'Decreto Pronvincial - ' + titleMatch[0].trim(),
                 content: decreeContent
             });
         }
     }
 
-    return decree;
+    return decrees;
 }
+
+function formatSynthesizedDecreeSection(sectionContent) {
+    const regex = /(Dto\. N° )(\d{4})(\d{2}-\d{2}-\d{2})/;
+    const lines = sectionContent.split('\n'); // divide text into lines
+    const formattedLines = [];
+
+    for (const line of lines) {
+        const match = line.match(regex);
+        if (match) {
+            formattedLines.push(`${match[1]}${match[2]}\n${match[3]}`);
+        } else {
+            formattedLines.push(line);
+        }
+    }
+
+    return formattedLines.join('\n');
+}
+
+function processSynthesizedDecrees(sectionContent) {
+    const formattedContent = formatSynthesizedDecreeSection(sectionContent);
+    const synthesizedDecrees = [];
+    const synthesizedDecreeRegex = /Dto\. N° \d{4}(.*?)(?=Dto\. N° \d{4}|$)/gs;
+    const decreeRegex = /Dto\. N° \d{4}\n/gs;
+
+    let match;
+    while ((match = synthesizedDecreeRegex.exec(formattedContent)) !== null) {
+        let synthesizedDecreeContent = match[0].trim();
+        let titleMatch;
+
+        while ((titleMatch = decreeRegex.exec(synthesizedDecreeContent)) !== null) {
+            synthesizedDecrees.push({
+                title: 'Auditoría Legislativa - ' + 'Decreto Sintetizado - ' + titleMatch[0].trim(),
+                content: synthesizedDecreeContent
+            });
+        }
+    }
+
+    return synthesizedDecrees;
+}
+
