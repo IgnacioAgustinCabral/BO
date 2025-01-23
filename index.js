50a9da7647ad24ed522c976f383e0a81642bb4e1
Ignacio Cabral <ignaciocabral97@gmail.com>
2025-01-23 19:25:16 -0300

    added puppeteer to scrape BO links from Rio Negro & Comodoro Rivadavia

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const parseSantaCruzPDF = require('./SantaCruz/index.js');
 const parseCaletaOliviaPDF = require('./CaletaOlivia/index.js');
 const parseComodoroRivadaviaPDF = require('./ComodoroRivadavia/index.js');
 const port = 4000;
+const puppeteer = require('puppeteer');
 const app = express();
 
 const parsers = {
@@ -32,6 +33,38 @@ app.get('/process-bulletin', async (req, res) => {
         console.log(error);
     }
 
+});
+
+app.get('/extract-links', async (req, res) => {
+    const {url, sources_id} = req.query;
+
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true,
+            timeout: 0,
+        });
+
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
+        await page.goto(url);
+
+        if (sources_id === '672') {
+            await page.waitForSelector('vaadin-grid-cell-content');
+            await page.evaluate(() => {
+                const element = document.querySelector('vaadin-grid-cell-content');
+                if (element) element.click();
+            });
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const data = await page.evaluate(() => `<html>${document.documentElement.innerHTML}</html>`);
+        await browser.close();
+        res.send(data);
+
+    } catch (err) {
+        res.status(500).send('Error occurred while processing the request.');
+    }
 });
 
 app.listen(port, () => {
